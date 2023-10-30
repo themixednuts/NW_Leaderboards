c74b3a17af6ce73e2831976d3cf5730551bea34b
Mixed Nuts <jonfonts@gmail.com>
2023-10-30 03:49:22 -0500

    edge test

--- src/lib/server/locales.ts
+++ src/lib/server/locales.ts
@@ -1,11 +1,11 @@
-import { JSDOM } from 'jsdom'
+// import { JSDOM } from 'jsdom'
 
 export async function getCraftingLocale(){
   const xml = await fetch(
   'https://raw.githubusercontent.com/new-world-tools/localization/main/javelindata_craftingcategories.loc.xml',
 ).then((res) => res.text())
 
-const { document } = new JSDOM(xml).window
+const { document } = window
 const strings = document.querySelectorAll('string')
 const craftingLocales: { [k: string]: string | null } = {}
 strings.forEach((ele) => {
