c551452ddb0238cdb93774c1010fdcede521ed24
Mixed Nuts <jonfonts@gmail.com>
2023-11-01 05:32:26 -0500

    feat: filter

--- src/routes/market/api/search/[item]/+server.ts
+++ src/routes/market/api/search/[item]/+server.ts
@@ -1,7 +1,14 @@
 import { db } from '$lib/server/db';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import type { Config } from '@sveltejs/adapter-vercel';
 
+export const config: Config = {
+    isr: {
+        expiration: 14400
+    },
+    runtime: 'nodejs18.x'
+}
 export const GET: RequestHandler = async ({ params: { item } }) => {
 
     const query = `
