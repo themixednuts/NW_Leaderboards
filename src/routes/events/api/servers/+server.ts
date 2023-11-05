10e1d03a9fff10ab2e8e5db651a345d19a84de62
Mixed Nuts <jonfonts@gmail.com>
2023-11-05 03:35:07 -0600

    feat: events api

---
+++ src/routes/events/api/servers/+server.ts
@@ -0,0 +1,22 @@
+import type { RequestHandler } from './$types';
+import { db } from '$lib/server/db';
+import { error, json } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async () => {
+    let result
+    let startTime = performance.now()
+    try {
+        result = await db.execute(`
+    -- EXPLAIN QUERY PLAN
+    SELECT DISTINCT server
+    FROM events
+    `)
+    } catch (e) {
+        console.log(e)
+        throw error(500, "Incorrect Request")
+    }
+    console.log('db timer - Servers: ', performance.now() - startTime, ' ms')
+    // console.log(result.rows)
+
+    return json({ servers: result.rows.map(row => row.server) })
+};
