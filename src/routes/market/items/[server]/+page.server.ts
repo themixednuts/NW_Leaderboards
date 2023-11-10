57d8b0dbbce74641af1c1d3fbe41ff6bd13a9a37
Mixed Nuts <jonfonts@gmail.com>
2023-11-10 01:51:09 -0600

    added streaming data from load

---
+++ src/routes/market/items/[server]/+page.server.ts
@@ -0,0 +1,10 @@
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ params: { server }, fetch }) => {
+
+    return {
+        streamed: {
+            items: fetch(`/market/api/items/${server}`).then(res => res.json())
+        }
+    };
+}) satisfies PageServerLoad;
