be61d22104e17f0d44b8115ed5165ba4a830c9de
Mixed Nuts <jonfonts@gmail.com>
2023-11-09 19:18:57 -0600

    removed bypass for now

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/kit/vite'
-import { BYPASS_TOKEN } from '$env/static/private'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +11,7 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({ runtime: 'nodejs18.x', isr: { expiration: 1800, bypassToken: BYPASS_TOKEN } }),
+    adapter: adapter({ runtime: 'nodejs18.x', isr: { expiration: 1800 } }),
   },
 
 }
