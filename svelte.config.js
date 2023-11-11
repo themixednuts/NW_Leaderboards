2d7cd6be2d25dcf9eb115d9e67c4b9fd810f25d0
Mixed Nuts <jonfonts@gmail.com>
2023-11-11 02:42:07 -0600

    streaming test

--- svelte.config.js
+++ svelte.config.js
@@ -10,8 +10,8 @@ const config = {
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({ runtime: 'nodejs18.x', isr: { expiration: 3600 } }),
+    // See https://kt.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
   },
 
 }
