4ae033995e9f343b9d6eadd159f88dab1daa211b
themixednuts <jonfonts@gmail.com>
2024-03-30 14:52:52 -0500

    this is really stupid im about to cry

--- src/routes/market/api/servers/+server.ts
+++ src/routes/market/api/servers/+server.ts
@@ -1,29 +1,30 @@
 import type { RequestHandler } from './$types';
-import { db } from '$lib/server/db';
+// import { db } from '$lib/server/db';
 import { error, json } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ setHeaders }) => {
 
 
 
-    setHeaders({
-        'cache-control': "public,s-maxage=9000"
-    })
+  setHeaders({
+    'cache-control': "public,s-maxage=9000"
+  })
 
-    let result
-    let startTime = performance.now()
-    try {
-        result = await db.execute(`
-    -- EXPLAIN QUERY PLAN
-    SELECT server
-    FROM server_metadata
-    `)
-    } catch (e) {
-        console.log(e)
-        error(500, "Incorrect Request");
-    }
-    console.log('db timer - Servers: ', performance.now() - startTime, ' ms')
-    // console.log(result.rows)
+  let result
+  let startTime = performance.now()
+  try {
+    //     result = await db.execute(`
+    // -- EXPLAIN QUERY PLAN
+    // SELECT server
+    // FROM server_metadata
+    // `)
+  } catch (e) {
+    console.log(e)
+    error(500, "Incorrect Request");
+  }
+  console.log('db timer - Servers: ', performance.now() - startTime, ' ms')
+  // console.log(result.rows)
 
-    return json({ servers: result.rows.map(row => row.server) })
+  // return json({ servers: result.rows.map(row => row.server) })
+  return json([])
 };
