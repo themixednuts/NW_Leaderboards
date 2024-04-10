c5a2e1685d0745ea00dbe44cfcc14d870f87d79a
themixednuts <jonfonts@gmail.com>
2024-04-10 01:03:49 -0500

    add ability to upload game.log backups

--- src/routes/settings/upload/schema.ts
+++ src/routes/settings/upload/schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 export const gameLogFormSchema = z.object({
   file: z.instanceof(File, { message: 'Please upload a file.' })
-    .refine(f => f.name === 'Game.log', 'Please upload Game.log file.')
+    .refine(f => /^Game.*\.log$/.test(f.name), 'Please upload Game.log or backup log file.')
     // .refine(f => f.type === 'text/x-log' || f.type === 'text/plain', 'Uploaded incorrect type of file.')
     .refine(file => file.size <= 1_000_000)
 })
