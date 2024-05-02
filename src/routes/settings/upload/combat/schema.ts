1dda394d9267e9a1e02f60669251e2b514e7438b
themixednuts <jonfonts@gmail.com>
2024-05-02 18:00:17 -0500

    refactor: increase upload limit for combat log

--- src/routes/settings/upload/combat/schema.ts
+++ src/routes/settings/upload/combat/schema.ts
@@ -5,7 +5,7 @@ export const formSchema = z.object({
   files: z.instanceof(File, { message: 'Please upload a file.' })
     // .refine(f => /^Game.*\.log$/.test(f.name), 'Please upload combat log files.')
     // .refine(f => f.type === 'text/x-log' || f.type === 'text/plain', 'Uploaded incorrect type of file.')
-    .refine(f => f.size <= 5_000_000, 'Please upload files less than 5Mb')
+    .refine(f => f.size <= 15_000_000, 'Please upload files less than 5Mb')
 })
 
 export type FormSchema = typeof formSchema
